fe08b5f8b9b421f8979fab379ce6009dba1634b9
Derfies <Derfies@users.noreply.github.com>
2024-09-20 07:15:23 +1000

    [EDIT] - types.py - Fixed copy issue.

--- propertygrid/types.py
+++ propertygrid/types.py
@@ -2,6 +2,7 @@ from PySide6.QtGui import QImage
 
 import sys
 if 'unittest' not in sys.modules.keys():
+
     # noinspection PyUnresolvedReferences
     from __feature__ import snake_case
 
@@ -16,6 +17,9 @@ class FilePathQImage:
 
     def __init__(self, file_path: str = ''):
         self.file_path = file_path
+
+    def __deepcopy__(self, memodict: dict = None):
+        return self.__class__(self.file_path)
 
     @property
     def file_path(self):
